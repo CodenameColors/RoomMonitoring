790495dfd01c28d1d154e3d3b685a486e83cb532
psychoticpolice <calling911x@gmail.com>
2018-10-04 17:04:10 -0400

    migrations: created the initdb script. pg database is ready

---
+++ Migrations/migrations/20181004150518_initdb.js
@@ -0,0 +1,24 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+	  knex.schema.createTableIfNotExists('sensor', (table) => {
+		  table.increments().primary() //primary key ID
+		  table.integer('person_id').unsigned().index().references('id').inTable('person')
+		  table.timestamp('entered_at').defaultTo(knex.fn.now()).notNullable()
+	  }),
+	  
+	  knex.schema.createTableIfNotExists('person', (table) => {
+		  table.increments().primary() //primary key ID
+		  table.string('fname').notNullable()
+		  table.string('lname').notNullable()
+		  table.string('group').notNullable()
+	  }),
+	])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+	  
+	  knex.schema.dropTableIfExists('sensor'),
+	  knex.schema.dropTableIfExists('person'),
+  ])
+};
